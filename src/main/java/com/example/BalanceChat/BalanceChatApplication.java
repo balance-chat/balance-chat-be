7caff385de3ce76099b2618af2a4cdd24dab0630
mingwan <ibini12@gmail.com>
2025-02-11 10:04:23 +0900

    chore : first commit

---
+++ src/main/java/com/example/BalanceChat/BalanceChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.BalanceChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceChatApplication.class, args);
+	}
+
+}
